4d738c40798bcd4352326538e932fc0ea8af6867
Bunde Uji <bundeuji@gmail.com>
2022-10-28 23:54:39 +0100

    style change to homepage and extra padding to contact page

--- src/components/ExploreBtn.js
+++ src/components/ExploreBtn.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 
 function ExploreBtn() {
     return (  
-        <Link to="/projects" className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-sky-700 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group my-4">
+        <Link to="/projects" className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-sky-700 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-slate-200 group my-4">
             <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-sky-600 group-hover:h-full"></span>
             <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
             <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -15,9 +15,9 @@ function HomePage() {
         <div className='h-[100vh] md:h-[110vh] w-full relative' style={{background: `url(${bg})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
             {/* <video src={bgVideo} autoPlay muted className='object-cover h-full w-full absolute opacity-95'></video> */}
             <div className={`w-full h-full flex flex-col justify-center items-center absolute transform translate-y-[-10%] animate__animated animate__fadeIn`}>
-                <h2 className='text-white text-3xl text font-bold mb-2 drop-shadow-xl'>Hi there,</h2>
-                <h1 className='text-white text-4xl text font-bold mb-2'>I'm <span className='text-sky-500'>Bunde Uji</span></h1>
-                <h2 className='text-white text-3xl text font-bold mb-2 cursor-pointer text-center'>Front-End Developer</h2>
+                <h2 className='text-slate-200 text-3xl text font-bold mb-2 drop-shadow-xl'>Hi there,</h2>
+                <h1 className='text-slate-200 text-4xl text font-bold mb-2'>I'm <span className='text-sky-500'>Bunde Uji</span></h1>
+                <h2 className='text-slate-200 text-3xl text font-bold mb-2 cursor-pointer text-center'>Front-End Developer</h2>
                 <ExploreBtn />
 
             </div>
